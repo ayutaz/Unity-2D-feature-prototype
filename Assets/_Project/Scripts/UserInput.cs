1b70c4821315bb7e880ce9ce5a902448f2bc6540
ayutaz <ka1357amnbpdr@gmail.com>
2022-04-28 01:56:20 +0900

    feat: ScriptからInput情報を取得

---
+++ Assets/_Project/Scripts/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Project
+{
+    public class UserInput : MonoBehaviour
+    {
+        private void Update()
+        {
+            if(Keyboard.current.sKey.wasPressedThisFrame)
+            {
+                Debug.Log("S key was pressed");
+            }
+            
+            if(Keyboard.current.sKey.wasReleasedThisFrame)
+            {
+                Debug.Log("S key was released");
+            }
+            
+            if(Mouse.current.leftButton.isPressed)  // isPressed is a property of the Mouse.current.leftButton
+            {
+                Debug.Log("Left mouse button is pressed");
+            }
+            
+        }
+    }
+}
